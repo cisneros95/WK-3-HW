2ed5e29f03d6857edc0bb5588f8a3ba5eb093fb3
Raul Cisneros <raul.f.cisneros@outlook.com>
2020-05-24 16:25:31 -0700

    HW Week 03 Problem 1 .SLN

--- Program.cs
+++ Program.cs
@@ -11,24 +11,82 @@ namespace IF_STATEMENTS
     {
         static void Main(string[] args)
         {
-        Start:
-            double num01;
-            double num02;
-
-            Console.Write("Enter a number to be divided: ");
-            num01 = Convert.ToInt32 (Console.ReadLine () );
-            Console.Write("Enter a number to divide by: ");
-            num02 = Convert.ToInt32(Console.ReadLine () );
 
 
-            Console.WriteLine( num01 + " divided by " + num02 + " is equal to  " + num01 / num02);
 
 
-            // Wait for the user to press a key. Then make empty space and start over.
+            Random numberGenerator = new Random();
+
+            Start:
+            int num01 = numberGenerator.Next(100,999); /// "numberGenerator.Next" will produce a random number
+            int num02 = numberGenerator.Next(100,999);
+
+
+            Console.WriteLine("What is ? \n   " + num01 +"\n + " + num02 + "\n============"); /// \n will create a new line
+            int answer = Convert.ToInt32(Console.ReadLine( ));
+
+            if (answer == num01 + num02)
+            {
+                Console.WriteLine("Congratulations  ! Your answer is correct.");
+            }
+            else
+            {
+
+                int responseIndex = numberGenerator.Next(100, 101);
+
+                switch (responseIndex) 
+                {
+                    case 1:
+                        Console.WriteLine("Are you even trying?");
+                        break;
+                    case 2:
+                        Console.WriteLine("The answer was incorrect.");
+                        break;
+                    default:
+                        Console.WriteLine("You can do better than that.");
+                        break;
+
+                }
+
+                
+            }
 
             Console.ReadKey();
-            Console.WriteLine();
-            goto Start; // Jumps to "Start:".
+            goto Start;
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Start:
+        //    double num01;
+        //    double num02;
+
+        //    Console.Write("Enter a number to be divided: ");
+        //    num01 = Convert.ToInt32 (Console.ReadLine () );
+        //    Console.Write("Enter a number to divide by: ");
+        //    num02 = Convert.ToInt32(Console.ReadLine () );
+
+
+        //    Console.WriteLine( num01 + " divided by " + num02 + " is equal to  " + num01 / num02);
+
+
+        //    // Wait for the user to press a key. Then make empty space and start over.
+
+        //    Console.ReadKey();
+        //    Console.WriteLine();
+        //    goto Start; // Jumps to "Start:".
 
 
 
